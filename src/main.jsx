945549357ec9569b1f77d1e7dfc001d69daa1028
wilobimerchandise-rgb <wilobimerchandise@gmail.com>
2026-08-10 10:34:04 +0100

    Initialize main.jsx with React and Invoice component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Invoice from './Invoice.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Invoice />
+  </React.StrictMode>,
+)
